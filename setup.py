8875db8a338edcf6f817c3c85e65cb47823fa548
Patrick Jayet <patrick.jayet@axa-tech.com>
2018-11-09 16:17:15 +0100

    Version bump to 1.4

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.md', 'r') as f:
     readme = f.read()
 setup(
     name='selfconsistency',
-    version='1.3',
+    version='1.4',
     packages=find_packages(exclude=['tests*']),
     license='Apache 2.0',
     description='Image Splice Detection via Learned Self-Consistency',
